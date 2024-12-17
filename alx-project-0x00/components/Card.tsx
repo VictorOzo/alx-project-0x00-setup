f2e087aed383f54762f5c3943256910914786a55
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-12-17 14:20:41 -0800

    functional components

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import HOUSE_IMAGE from "@/public/assets/house.png";
 import STAR_IMAGE from "@/public/assets/star.png";
+
 import React from "react";
 import Pill from "./Pill";
 
